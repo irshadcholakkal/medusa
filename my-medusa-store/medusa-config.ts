f40eac5b2ec36bb43b7e90ca10e11c07df264113
Irshad Cholakkal <irshadsh12773@gmail.com>
2025-12-19 02:37:22 +0530

    all reseted

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -1,31 +1,10 @@
-// import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
-// loadEnv(process.env.NODE_ENV || 'development', process.cwd())
-
-// module.exports = defineConfig({
-//   projectConfig: {
-//     databaseUrl: process.env.DATABASE_URL,
-//     http: {
-//       storeCors: process.env.STORE_CORS!,
-//       adminCors: process.env.ADMIN_CORS!,
-//       authCors: process.env.AUTH_CORS!,
-//       jwtSecret: process.env.JWT_SECRET || "supersecret",
-//       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-//     }
-//   }
-// })
-
-
-
-// import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-import { loadEnv, defineConfig } from '@medusajs/utils'
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL, 
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -33,19 +12,6 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  },
-  modules: [
-    {
-      resolve: "@medusajs/medusa/event-bus-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/cache-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-  ],
+  }
 })
+
